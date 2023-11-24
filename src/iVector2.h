2d985366e4ce66358ee2b349396cb39f5912691a
AlOwain <abdulaziz@alowain.dev>
2023-11-24 22:16:25 +0300

    Changed spacing in iVector2.h, only affects style of file

--- src/iVector2.h
+++ src/iVector2.h
@@ -9,10 +9,12 @@ public:
     iVector2 operator-(iVector2 vector);
     iVector2 operator*(iVector2 vector);
     iVector2 operator/(iVector2 vector);
+    
     iVector2 operator+(int number);
     iVector2 operator-(int number);
     iVector2 operator*(int number);
     iVector2 operator/(int number);
+
     void operator+=(iVector2 vector);
     void operator-=(iVector2 vector);
     void operator*=(iVector2 vector);
